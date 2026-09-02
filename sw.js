47e5b5aa38a14940cd8eb521630672ae921a033e
Luke JB Atelier <luke-jb-atelier@users.noreply.github.com>
2026-09-02 05:54:10 +0200

    Release v9.9 unified workflow steps

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "vykonak-kalkulator-v98";
+const CACHE_NAME = "vykonak-kalkulator-v99";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=98",
-  "./app.js?v=98",
+  "./styles.css?v=99",
+  "./app.js?v=99",
   "./manifest.webmanifest",
   "./icon.svg",
   "./icon-192.png",
